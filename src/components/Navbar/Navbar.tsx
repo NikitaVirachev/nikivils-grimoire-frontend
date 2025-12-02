4c33427da570a3371f2719ec193eff6c8af97428
NikitaVirachev <guardsolder@gmail.com>
2025-12-02 23:09:23 +0300

    feat #4: create Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+import marbleTexture from '../../assets/img/textures/marble.gif';
+
+const border = 0.5;
+
+const Wrapper = styled.nav`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 0.5rem;
+
+  position: relative;
+  padding: ${border + 0.2}rem;
+  grid-column: -1 / 1;
+
+  background: var(--tertiary-color);
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    padding: ${border}rem;
+    pointer-events: none;
+
+    background: url(${marbleTexture}) 0 0 / 5rem 5rem repeat;
+
+    mask:
+      linear-gradient(#000 0 0) content-box,
+      linear-gradient(#000 0 0);
+    -webkit-mask:
+      linear-gradient(#000 0 0) content-box,
+      linear-gradient(#000 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
+`;
+
+const NavItem = styled(NavLink)`
+  font-family: 'Metamorphous', serif;
+  font-size: var(--small);
+  color: var(--tertiary-color);
+
+  height: 2.6rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: white;
+`;
+
+const Navbar = () => (
+  <Wrapper>
+    <NavItem to='/'>Home</NavItem>
+    <NavItem to='/about'>About</NavItem>
+    <NavItem to='/blog'>Blog</NavItem>
+    <NavItem to='/gallery'>Gallery</NavItem>
+  </Wrapper>
+);
+
+export default Navbar;
