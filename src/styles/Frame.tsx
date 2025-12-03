ab56510faf3d9f0997cd8ec974e4adcb49e72ec2
virachev.n <virachev.n@energosoft.net>
2025-12-03 14:49:47 +0300

    feat #4: create frame styles

---
+++ src/styles/Frame.tsx
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+import secondaryFrameUrl from '../assets/img/frame-seconadry.svg';
+
+const Frame = css`
+  border-image-source: url(${secondaryFrameUrl});
+  border-image-slice: 11 15;
+  border-image-width: 11px 15px;
+  border-image-repeat: stretch;
+`;
+
+export default Frame;
