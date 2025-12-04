91e1930eddb3e1f3b918ee09d311d4bdbcb20b65
NikitaVirachev <guardsolder@gmail.com>
2025-12-04 18:05:29 +0300

    feat #4: create primarly frame

--- src/styles/Frame.tsx
+++ src/styles/Frame.tsx
@@ -1,12 +1,18 @@
 import { css } from 'styled-components';
 
+import primarlyFrameUrl from '../assets/img/frame-primarly.svg';
 import secondaryFrameUrl from '../assets/img/frame-seconadry.svg';
 
-const Frame = css`
+export const SecondaryFrame = css`
   border-image-source: url(${secondaryFrameUrl});
   border-image-slice: 11 15;
   border-image-width: 11px 15px;
   border-image-repeat: stretch;
 `;
 
-export default Frame;
+export const PrimarlyFrame = css`
+  border-image-source: url(${primarlyFrameUrl});
+  border-image-slice: 11 15;
+  border-image-width: 11px 15px;
+  border-image-repeat: stretch;
+`;
