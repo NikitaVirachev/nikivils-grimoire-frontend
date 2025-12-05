d59fb9194618ff9f097c5f99f4917f00eeead127
NikitaVirachev <guardsolder@gmail.com>
2025-12-05 22:13:11 +0300

    feat #4: add chain link background in LeftChain

--- src/layouts/MainContent.tsx
+++ src/layouts/MainContent.tsx
@@ -72,6 +72,19 @@ const Content = styled.section`
 
 const LeftChain = styled.div`
   width: 2.4rem;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background-image: url(${chainLinkUrl});
+    background-repeat: repeat-y;
+    background-position: center;
+  }
 `;
 
 const Column = styled.div`
