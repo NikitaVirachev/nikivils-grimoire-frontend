c5acfd6a9fda23209f85b8eef3a27d5006021962
NikitaVirachev <guardsolder@gmail.com>
2025-12-07 18:35:30 +0300

    feat #4: create Sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,51 @@
+import type { ReactNode } from 'react';
+import styled from 'styled-components';
+
+import PictureFrame from '../../styles/PictureFrame';
+import FrameTitle from '../FrameTitle/FrameTitle';
+
+import sidebarBackgroundUrl from '../../assets/img/backgrounds/sidebar-background.jpg';
+
+const SidebarLayout = styled.aside`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+
+  ${PictureFrame};
+`;
+
+const Column = styled.div`
+  flex-grow: 1;
+  padding: 0 0.2rem 0.2rem 0.2rem;
+
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${sidebarBackgroundUrl});
+    background-repeat: no-repeat;
+    background-position: center -25rem;
+    opacity: 0.1;
+  }
+`;
+
+interface SidebarProps {
+  title: string;
+  className?: string;
+  children: ReactNode;
+}
+
+const Sidebar = ({ title, className, children }: SidebarProps) => (
+  <SidebarLayout className={className}>
+    <FrameTitle>{title}</FrameTitle>
+    <Column>{children}</Column>
+  </SidebarLayout>
+);
+
+export default Sidebar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 import MainContent from '../layouts/MainContent';
+import Sidebar from '../components/Sidebar/Sidebar';
 
 const StyledMainContent = styled(MainContent)`
   grid-column: col-start 1 / col-end 3;
 `;
 
-const HomePage = () => <StyledMainContent title='News'>Home</StyledMainContent>;
+const StyledSidebar = styled(Sidebar)``;
+
+const HomePage = () => (
+  <>
+    <StyledMainContent title='News'>Home</StyledMainContent>
+    <StyledSidebar title='Explore'>Sidebar</StyledSidebar>
+  </>
+);
 
 export default HomePage;
