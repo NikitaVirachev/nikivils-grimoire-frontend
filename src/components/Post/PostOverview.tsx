8442c0ab91223ff66bb85873418b244f65b52a75
NikitaVirachev <guardsolder@gmail.com>
2025-12-07 23:26:59 +0300

    feat #4: create post overview component

---
+++ src/components/Post/PostOverview.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+import Bat from '../Icon/Bat';
+
+const Post = styled.article`
+  font-family: 'Coelacanth', serif;
+  color: var(--secondary-tp-color);
+  padding: 0.5rem 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const Header = styled.header`
+  display: flex;
+  gap: 0.5rem;
+  padding-left: 2rem;
+`;
+
+const Title = styled.h4`
+  font-size: var(--h4);
+
+  flex-grow: 1;
+`;
+
+const PostBody = styled.p`
+  font-size: var(--p);
+  padding: 0 2rem;
+`;
+
+const Border = styled.footer`
+  font-family: 'Elder Futhark';
+  font-size: 0.8rem;
+  color: var(--primarly-color);
+`;
+
+interface PostOverviewProps {
+  title: string;
+  overview: string;
+}
+
+const PostOverview = ({ title, overview }: PostOverviewProps) => (
+  <Post>
+    <Content>
+      <Header>
+        <Bat title='Bat' />
+        <Title>{title}</Title>
+      </Header>
+      <PostBody>{overview}</PostBody>
+    </Content>
+
+    <Border>
+      LoremipsumdolorsitametconsecteturSedrisuseuismodmalesuadaelementum.MaecenasincommodoametlacusantecursusFringillafelissemperenimv.UHK
+    </Border>
+  </Post>
+);
+
+export default PostOverview;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import MainContent from '../layouts/MainContent';
 import Sidebar from '../components/Sidebar/Sidebar';
+import PostOverview from '../components/Post/PostOverview';
 
 const StyledMainContent = styled(MainContent)`
   grid-column: col-start 1 / col-end 3;
@@ -9,9 +10,56 @@ const StyledMainContent = styled(MainContent)`
 
 const StyledSidebar = styled(Sidebar)``;
 
+const posts = [
+  {
+    id: 1,
+    title: 'New post!',
+    overview:
+      'Lorem ipsum dolor sit amet consectetur. Pellentesque nisi placerat ultrices ac amet dui et pellentesque. Tellus commodo eu interdum nec nullam. Leo congue feugiat sed bibendum vel amet. In habitasse et viverra nibh risus tempor morbi arcu cras. In molestie commodo sit aliquam montes egestas semper. Sollicitudin aenean convallis integer eget. Nunc enim sit porta et maecenas facilisis. Amet et in ligula sed pharetra urna bibendum mauris accumsan. Sed eleifend ipsum urna consectetur sed amet laoreet habitant tempus.',
+  },
+  {
+    id: 0,
+    title: 'My first post!',
+    overview:
+      'Lorem ipsum dolor sit amet consectetur. Elit eget eget scelerisque nibh. Natoque platea felis quis aliquam pharetra eleifend pulvinar malesuada erat. Amet sit tempus nulla lacus sed tellus. In ac ipsum dictum et. Posuere volutpat orci cursus habitant magna cursus habitasse. Cursus nisl nascetur vel sit. Tellus erat consequat enim facilisi at fames volutpat.',
+  },
+  {
+    id: 2,
+    title: 'My first post!',
+    overview:
+      'Lorem ipsum dolor sit amet consectetur. Elit eget eget scelerisque nibh. Natoque platea felis quis aliquam pharetra eleifend pulvinar malesuada erat. Amet sit tempus nulla lacus sed tellus. In ac ipsum dictum et. Posuere volutpat orci cursus habitant magna cursus habitasse. Cursus nisl nascetur vel sit. Tellus erat consequat enim facilisi at fames volutpat.',
+  },
+  {
+    id: 3,
+    title: 'My first post!',
+    overview:
+      'Lorem ipsum dolor sit amet consectetur. Elit eget eget scelerisque nibh. Natoque platea felis quis aliquam pharetra eleifend pulvinar malesuada erat. Amet sit tempus nulla lacus sed tellus. In ac ipsum dictum et. Posuere volutpat orci cursus habitant magna cursus habitasse. Cursus nisl nascetur vel sit. Tellus erat consequat enim facilisi at fames volutpat.',
+  },
+  {
+    id: 4,
+    title: 'My first post!',
+    overview:
+      'Lorem ipsum dolor sit amet consectetur. Elit eget eget scelerisque nibh. Natoque platea felis quis aliquam pharetra eleifend pulvinar malesuada erat. Amet sit tempus nulla lacus sed tellus. In ac ipsum dictum et. Posuere volutpat orci cursus habitant magna cursus habitasse. Cursus nisl nascetur vel sit. Tellus erat consequat enim facilisi at fames volutpat.',
+  },
+  {
+    id: 5,
+    title: 'My first post!',
+    overview:
+      'Lorem ipsum dolor sit amet consectetur. Elit eget eget scelerisque nibh. Natoque platea felis quis aliquam pharetra eleifend pulvinar malesuada erat. Amet sit tempus nulla lacus sed tellus. In ac ipsum dictum et. Posuere volutpat orci cursus habitant magna cursus habitasse. Cursus nisl nascetur vel sit. Tellus erat consequat enim facilisi at fames volutpat.',
+  },
+];
+
 const HomePage = () => (
   <>
-    <StyledMainContent title='News'>Home</StyledMainContent>
+    <StyledMainContent title='News'>
+      {posts.map((post) => (
+        <PostOverview
+          key={post.id}
+          title={post.title}
+          overview={post.overview}
+        />
+      ))}
+    </StyledMainContent>
     <StyledSidebar title='Explore'>Sidebar</StyledSidebar>
   </>
 );
