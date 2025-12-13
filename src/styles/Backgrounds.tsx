887ee67a6e58e8574774479ffd45562e7176968c
NikitaVirachev <guardsolder@gmail.com>
2025-12-13 22:01:01 +0300

    feat #4: add velvet background

--- src/styles/Backgrounds.tsx
+++ src/styles/Backgrounds.tsx
@@ -1,7 +1,12 @@
 import { css } from 'styled-components';
 
 import marbleTexture from '../assets/img/textures/marble.gif';
+import velvetTexture from '../assets/img/textures/velvet.jpg';
 
 export const MarbleBackground = css`
   background: url(${marbleTexture}) 0 0 / 5rem 5rem repeat;
+`;
+
+export const VelvetBackground = css`
+  background: url(${velvetTexture}) 0 0 / 2.1rem 2.1rem repeat;
 `;
