73d562ebdcdc4f8550fd74a96b54d499be6f111a
NikitaVirachev <guardsolder@gmail.com>
2025-12-25 14:43:48 +0300

    feat #4: implement respond mixin

---
+++ src/styles/Mixins.tsx
@@ -0,0 +1,33 @@
+import { css } from 'styled-components';
+
+type Breakpoint = 'phone' | 'tab-landscape' | 'tab-portrait' | 'desktop';
+type CssBlock = ReturnType<typeof css>;
+
+export const respond = (breakpoint: Breakpoint, content: CssBlock) => {
+  switch (breakpoint) {
+    case 'phone':
+      return css`
+        @media only screen and (width < 800px) {
+          ${content};
+        }
+      `;
+    case 'tab-landscape':
+      return css`
+        @media only screen and (width < 1095px) {
+          ${content};
+        }
+      `;
+    case 'tab-portrait':
+      return css`
+        @media only screen and (width < 1440px) {
+          ${content};
+        }
+      `;
+    case 'desktop':
+      return css`
+        @media only screen and (width >= 1440px) {
+          ${content};
+        }
+      `;
+  }
+};
