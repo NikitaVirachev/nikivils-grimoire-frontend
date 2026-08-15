4bc481848cc16233f7b87cdb594fb088cd381220
NikitaVirachev <guardsolder@gmail.com>
2026-08-15 20:00:05 +0300

    feat #6: add routes for about sub-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, replace } from 'react-router-dom';
 
 import GlobalStyle from './styles/GlobalStyle';
 
 import MainLayout from './layouts/MainLayout';
 
 import Home from './pages/Home';
-import About from './pages/About';
+import About from './pages/About/About';
 import Blog from './pages/Blog';
+import AboutMe from './pages/About/AboutMe';
+import FAQ from './pages/About/FAQ';
+import Favorite from './pages/About/Favorite';
+import Unfavorite from './pages/About/Unfavorite';
 
 const router = createBrowserRouter([
   {
@@ -14,7 +18,17 @@ const router = createBrowserRouter([
 
     children: [
       { index: true, Component: Home },
-      { path: 'about', Component: About },
+      {
+        path: 'about',
+        Component: About,
+        children: [
+          { index: true, loader: () => replace('me') },
+          { path: 'me', Component: AboutMe },
+          { path: 'faq', Component: FAQ },
+          { path: 'favorite', Component: Favorite },
+          { path: 'unfavorite', Component: Unfavorite },
+        ],
+      },
       { path: 'blog', Component: Blog },
     ],
   },
